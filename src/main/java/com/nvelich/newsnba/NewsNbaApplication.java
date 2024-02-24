ccdd56d9d1a0e5f33db131bb80d1c08039b1f595
Nikita <velich.nik@mail.ru>
2024-02-24 19:05:57 +0300

    initial setup jersey spring application

---
+++ src/main/java/com/nvelich/newsnba/NewsNbaApplication.java
@@ -0,0 +1,13 @@
+package com.nvelich.newsnba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsNbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsNbaApplication.class, args);
+    }
+
+}
