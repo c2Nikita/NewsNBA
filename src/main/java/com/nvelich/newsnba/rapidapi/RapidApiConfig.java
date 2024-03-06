ae673500e747da86b926c4a210f0c993409dfc46
c2Nikita <104105609+c2Nikita@users.noreply.github.com>
2024-03-06 21:58:48 +0300

    Update RapidApiConfig.java

--- src/main/java/com/nvelich/newsnba/rapidapi/RapidApiConfig.java
+++ src/main/java/com/nvelich/newsnba/rapidapi/RapidApiConfig.java
@@ -7,6 +7,6 @@ public class RapidApiConfig {
     }
 
     static final String URL = "https://nba-latest-news.p.rapidapi.com";
-    static final String KEY = "84fca61f44msh092bb414897bfa1p117101jsn3e81cf77c585";
+    static final String KEY = "Your rapidapi key";
     static final String HOST = "nba-latest-news.p.rapidapi.com";
 }
